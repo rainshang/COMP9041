2378a4ab638b49dbc3eabd9774a069d15cb9fff4
Ethan <rainshanghsu@qq.com>
2018-09-20 13:15:18 +1000

    finish 'legit.pl add <filenames>'

--- asst/asst01/legit.pl
+++ asst/asst01/legit.pl
@@ -1,20 +1,31 @@
 #!/usr/bin/perl -w
+use autodie;
 use File::Basename;
+use Digest::SHA qw(sha1_hex);
+use experimental 'smartmatch';
 
 $LE_GIT_DIR = ".legit";
 
 $LE_GIT_OBJECTS_DIR = $LE_GIT_DIR."/objects";
 $LE_GIT_REFS_DIR = $LE_GIT_DIR."/refs";
+$LE_GIT_INDEX = $LE_GIT_DIR."/index";
 
 $LE_GIT_REFS_HEADS_DIR = $LE_GIT_REFS_DIR."/heads";
 
-sub string2file {
-    my ($string, $file) = @_;
-    open(my $fh, '>', $file) or die;
-    print $fh $string;
+sub writeFile {
+    my ($data, $file) = @_;
+    open my $fh, '>', $file;
+    print $fh $data;
     close $fh;
 }
 
+sub readFile {
+    my ($file) = @_;
+    open my $fh, '<', $file;
+    read $fh, my $data, -s $fh;
+    close $fh;
+    return $data;
+}
 
 sub checkGitDir {
     if (!-d $LE_GIT_DIR) {
@@ -22,9 +33,50 @@ sub checkGitDir {
     }
 }
 
+sub readIndex {
+    if (!-e $LE_GIT_INDEX) {
+        return;
+    } else {
+        open my $fh, '<', $LE_GIT_INDEX;
+        my @index = ();
+        while (my $line = <$fh>) {
+            chomp $line;
+            push @index, $line;
+        }
+        close $fh; 
+        return @index;
+    }
+}
 
-if (@ARGV == 1) {
-    if ("init" eq $ARGV[0]) {
+# sha-1 conflict filename
+sub writeIndex {
+    my @index = @_;
+    open my $fh, '>', $LE_GIT_INDEX;
+    foreach my $record (@index) {
+        print $fh "$record\n";
+    }
+    close $fh;
+}
+
+sub hashObject {
+    my ($file) = @_;
+    my $data = readFile($file);
+    my $digest = sha1_hex($data);
+    my $a = substr($digest, 0, 2);
+    my $b = substr($digest, 2);
+    if (!-d $LE_GIT_OBJECTS_DIR."/$a") {
+        mkdir $LE_GIT_OBJECTS_DIR."/$a";
+    }
+    if (!-e $LE_GIT_OBJECTS_DIR."/$a/$b") {
+        writeFile($data, $LE_GIT_OBJECTS_DIR."/$a/$b");
+    }
+    return $digest;
+}
+
+if (@ARGV) {
+    $command = $ARGV[0];
+    # legit init
+    if ("init" eq $command) {
         if (-d $LE_GIT_DIR) {
             die basename($0).": error: $LE_GIT_DIR already exists\n";
         } else {
@@ -32,12 +84,49 @@ if (@ARGV == 1) {
             mkdir($LE_GIT_OBJECTS_DIR) and
             mkdir($LE_GIT_REFS_DIR) and
             mkdir($LE_GIT_REFS_HEADS_DIR) and
-            string2file("ref: refs/heads/master\n", $LE_GIT_DIR."/HEAD") and
+            writeFile("ref: refs/heads/master\n", $LE_GIT_DIR."/HEAD") and
             print("Initialized empty legit repository in $LE_GIT_DIR\n");
         }
     }
-} elsif (@ARGV >= 1) {
-    checkGitDir();
-    $n = @ARGV;
-    print("$n\n")
+    # legit ls-files --stage
+    elsif ("ls-files" eq $command) {
+        checkGitDir();
+        my @index = readIndex();
+        if (my $option = $ARGV[1]) {
+            if ("-s" eq $option or "--stage" eq $option) {
+                map{print "$_\n"} @index; 
+            }
+        } else {
+            foreach my $record (@index) {
+                my $filename = (split / /, $record)[2];
+                print("$filename\n");
+            }
+        }
+    }
+    # legit.pl add <filenames>
+    elsif ("add" eq $command) {
+        checkGitDir();
+        die basename($0).": error: nothing added.\nMaybe you wanted to say 'git add .'?\n" if (@ARGV < 2);
+        my @files = @ARGV[1..$#ARGV];
+        my @index = readIndex();
+        my @not_changed = ();
+        foreach my $record (@index) {
+            my ($sha_1, $conflict, $filename) = split(/ /, $record);
+            if ($filename !~ @files) {
+                push(@not_changed, $record);
+            }
+        }
+        @index = ();
+        push(@index, @not_changed);
+        foreach my $file (@files) {
+            if ($file =~ /^[a-zA-Z0-9][a-zA-Z0-9.-_]*/) {
+                $sha1 = hashObject($file);
+                my $record = "$sha1 0 $file";
+                push(@index, $record);
+            } else {
+                die basename($0).": error: invalid filename '$file'\n";
+            }
+        }
+        writeIndex(@index);
+    }
 }
