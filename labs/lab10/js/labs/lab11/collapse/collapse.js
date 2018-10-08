e5c0ca62ac7830b7a901ea3c4d6eae5e3b6731ac
Ethan <rainshanghsu@qq.com>
2018-10-08 13:48:00 +1100

    hehe

--- labs/lab10/js/labs/lab11/collapse/collapse.js
+++ labs/lab10/js/labs/lab11/collapse/collapse.js
@@ -1,8 +1,18 @@
 (function() {
   'use strict';
   // TODO: Write some js
-  let clickListener = () =>{
-
+  let clickListener = (btn) => {
+    let icon = btn.getElementsByTagName('i')[0];
+    let i = icon.id.substring(5);
+    if ('expand_less' == icon.textContent) {
+      document.getElementById(`item-${i}-content`).style.display = 'none';
+      icon.textContent = 'expand_more';
+    } else {
+      document.getElementById(`item-${i}-content`).style.display = 'block';
+      icon.textContent = 'expand_less';
+    }
   }
-  document.getElementById('item-1-content').innerText='草泥马'
+  Array.from(document.getElementsByClassName('expand-collapse-btn')).forEach(btn => {
+    btn.addEventListener('click', () => clickListener(btn));
+  });
 }());
